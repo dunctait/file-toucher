f66a0b78e6a19b8b3b44470d495c8a5f8715612c
dunctait <dunctait@hotmail.com>
2016-05-23 20:21:20 +0100

    begun styling

--- FileToucher/FileToucherView.xaml.cs
+++ FileToucher/FileToucherView.xaml.cs
@@ -1,29 +1,227 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows;
-using WPF.Themes;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+
 
 namespace FileToucher
 {
     /// <summary>
     /// Interaction logic for FileToucherView.xaml
     /// </summary>
-    public partial class FileToucherView : Window
+    public partial class FileToucherView
     {
+
+        private bool _restoreIfMove;
+
         public FileToucherView()
         {
-
-            // Use nicer Aero theme
-            var uri = new Uri("PresentationFramework.Aero;V3.0.0.0;31bf3856ad364e35;component\\themes/aero.normalcolor.xaml", UriKind.Relative);
-            try
-            {
-                Resources.MergedDictionaries.Add(Application.LoadComponent(uri) as ResourceDictionary);
-            }
-            catch
-            {
-                // couldn't apply theme... leave it to stay as default
-            }
-
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Minimize Button_Clicked
+        /// </summary>
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void RestoreButton_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchWindowState();
+        }
+
+        private void SwitchWindowState()
+        {
+            switch (WindowState)
+            {
+                case WindowState.Normal:
+                    {
+                        WindowState = WindowState.Maximized;
+                        break;
+                    }
+                case WindowState.Maximized:
+                    {
+                        WindowState = WindowState.Normal;
+                        break;
+                    }
+            }
+        }
+
+        private void TitleBarMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if ((ResizeMode == ResizeMode.CanResize) || (ResizeMode == ResizeMode.CanResizeWithGrip))
+                {
+                    SwitchWindowState();
+                }
+
+                return;
+            }
+
+            if (WindowState == WindowState.Maximized)
+            {
+                _restoreIfMove = true;
+                return;
+            }
+
+            DragMove();
+        }
+
+        private void TitleBarMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _restoreIfMove = false;
+        }
+
+        private void TitleBarMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_restoreIfMove) return;
+            _restoreIfMove = false;
+
+            var percentHorizontal = e.GetPosition(this).X / ActualWidth;
+            var targetHorizontal = RestoreBounds.Width * percentHorizontal;
+
+            var percentVertical = e.GetPosition(this).Y / ActualHeight;
+            var targetVertical = RestoreBounds.Height * percentVertical;
+
+            WindowState = WindowState.Normal;
+
+            POINT lMousePosition;
+            GetCursorPos(out lMousePosition);
+
+            Left = lMousePosition.X - targetHorizontal;
+            Top = lMousePosition.Y - targetVertical;
+
+            DragMove();
+        }
+
+        #region IgnorableCode
+
+        private void Window_SourceInitialized(object sender, EventArgs e)
+        {
+            IntPtr mWindowHandle = (new WindowInteropHelper(this)).Handle;
+            HwndSource.FromHwnd(mWindowHandle).AddHook(new HwndSourceHook(WindowProc));
+        }
+
+        private static System.IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case 0x0024:
+                    WmGetMinMaxInfo(hwnd, lParam);
+                    break;
+            }
+
+            return IntPtr.Zero;
+        }
+
+        private static void WmGetMinMaxInfo(System.IntPtr hwnd, System.IntPtr lParam)
+        {
+            POINT lMousePosition;
+            GetCursorPos(out lMousePosition);
+
+            IntPtr lPrimaryScreen = MonitorFromPoint(new POINT(0, 0), MonitorOptions.MONITOR_DEFAULTTOPRIMARY);
+            MONITORINFO lPrimaryScreenInfo = new MONITORINFO();
+            if (GetMonitorInfo(lPrimaryScreen, lPrimaryScreenInfo) == false)
+            {
+                return;
+            }
+
+            IntPtr lCurrentScreen = MonitorFromPoint(lMousePosition, MonitorOptions.MONITOR_DEFAULTTONEAREST);
+
+            MINMAXINFO lMmi = (MINMAXINFO) Marshal.PtrToStructure(lParam, typeof (MINMAXINFO));
+
+            if (lPrimaryScreen.Equals(lCurrentScreen) == true)
+            {
+                lMmi.ptMaxPosition.X = lPrimaryScreenInfo.rcWork.Left;
+                lMmi.ptMaxPosition.Y = lPrimaryScreenInfo.rcWork.Top;
+                lMmi.ptMaxSize.X = lPrimaryScreenInfo.rcWork.Right - lPrimaryScreenInfo.rcWork.Left;
+                lMmi.ptMaxSize.Y = lPrimaryScreenInfo.rcWork.Bottom - lPrimaryScreenInfo.rcWork.Top;
+            }
+            else
+            {
+                lMmi.ptMaxPosition.X = lPrimaryScreenInfo.rcMonitor.Left;
+                lMmi.ptMaxPosition.Y = lPrimaryScreenInfo.rcMonitor.Top;
+                lMmi.ptMaxSize.X = lPrimaryScreenInfo.rcMonitor.Right - lPrimaryScreenInfo.rcMonitor.Left;
+                lMmi.ptMaxSize.Y = lPrimaryScreenInfo.rcMonitor.Bottom - lPrimaryScreenInfo.rcMonitor.Top;
+            }
+
+            Marshal.StructureToPtr(lMmi, lParam, true);
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetCursorPos(out POINT lpPoint);
+
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr MonitorFromPoint(POINT pt, MonitorOptions dwFlags);
+
+        private enum MonitorOptions : uint
+        {
+            MONITOR_DEFAULTTONULL = 0x00000000,
+            MONITOR_DEFAULTTOPRIMARY = 0x00000001,
+            MONITOR_DEFAULTTONEAREST = 0x00000002
+        }
+
+
+        [DllImport("user32.dll")]
+        private static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public POINT(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        };
+
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class MONITORINFO
+        {
+            public int cbSize = Marshal.SizeOf(typeof (MONITORINFO));
+            public RECT rcMonitor = new RECT();
+            public RECT rcWork = new RECT();
+            public int dwFlags = 0;
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left, Top, Right, Bottom;
+
+            public RECT(int left, int top, int right, int bottom)
+            {
+                this.Left = left;
+                this.Top = top;
+                this.Right = right;
+                this.Bottom = bottom;
+            }
+        }
+
+        #endregion IgnorableCode
     }
 }
